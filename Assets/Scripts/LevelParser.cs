c6f24710a6ba1197d122cb15539a9c54505bd668
Tommy Tran <Tommyht1@uci.edu>
2015-01-24 09:49:53 -0800

    Level Parser Finished

--- Assets/Scripts/LevelParser.cs
+++ Assets/Scripts/LevelParser.cs
@@ -6,11 +6,12 @@ using System.Text;
 
 public enum TileType{EMPTY,BLOCK,ITEM};
 public class LevelParser : MonoBehaviour {
-	public GameObject[,] levelMap;
+	public TileType[,] levelMap;
 	public bool blah;
 	// Use this for initialization
 	void Start () {
-
+        ParseLevel("test.txt");
+        //toString();
 	}
 	
 	// Update is called once per frame
@@ -30,22 +31,30 @@ public class LevelParser : MonoBehaviour {
 			{
 				string line;
 
-				do
-				{
 					line = file.ReadLine();
 					if (line != null)
 					{
 						string[] tile =  line.Split(' ');
 						if(tile.Length > 0)
 						{
-
+                            //get header info
+                           int height = int.Parse(tile[0]);
+                           int width = int.Parse(tile[1]);
+                           levelMap = new TileType[height, width];
 						}
 					}
+                    //start reading in the rest of the file into the thing;
+                    for (int i = 0; i < levelMap.GetLength(0)/* height */; i++)
+                    {
+                        line = file.ReadLine();
+                        string[] tile = line.Split(' ');
+                        for(int j = 0; j < levelMap.GetLength(1) /*width*/; j++)
+                        {
+                            levelMap[i, j] = (TileType)int.Parse(tile[j]);
+                        }
+                    }
 				}
-				while(line != null);
 				file.Close();
-			}
-
 		}
 		catch (System.Exception e)
 		{
@@ -92,6 +101,7 @@ public class LevelParser : MonoBehaviour {
 			{
 				//if(levelMap[i,j] != null)
 				//Debug.Log(levelMap[i,j].mX + " " + levelMap[i,j].mY + " " + levelMap[i,j].mText.ToString());
+               // Debug.Log(levelMap[i, j].ToString());
 			}
 		}
 	}
