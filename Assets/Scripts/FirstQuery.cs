7abc4cf65cfe2b593c297a3e64c999cbbbf23952
KTZimmerle <kzimmerl@uci.edu>
2015-01-24 00:13:42 -0800

    Query & Response Web Requests

---
+++ Assets/Scripts/FirstQuery.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstQuery : MonoBehaviour {
+
+	public string url = "http://www.backworlds.com/whatnow/index.php?id=whatnow123";
+	public ResponseQuery something;
+
+	IEnumerator DecisionTime()
+	{
+		WWW wwwQuery = new WWW (url);
+		yield return wwwQuery;
+	}
+}
