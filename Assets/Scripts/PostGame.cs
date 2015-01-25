ffb739dda9a1c2855fb3b6e66870d8d136446a16
KTZimmerle <kzimmerl@uci.edu>
2015-01-25 11:18:02 -0800

    Changed QuitGame to PostGame
    includes restart button

--- Assets/Scripts/PostGame.cs
+++ Assets/Scripts/PostGame.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class QuitGame : MonoBehaviour {
+public class PostGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			Application.LoadLevel(1);
+		}
+
 		if(Input.GetKeyDown(KeyCode.Escape))
 		{
 			Application.Quit();
