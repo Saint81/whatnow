15fba6fcb2387a3802ae7afb0fb2f51b33927237
KTZimmerle <kzimmerl@uci.edu>
2015-01-25 12:40:56 -0800

    WinTrigger added
    Plan is to attach it to EndRoom prefab

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -9,8 +9,17 @@ public class HealthSystem : MonoBehaviour {
 	public int sweapons = 0;
 	public int mtools = 10;
 	public int mweapons = 10;
-	//public DeathByWinning explode;
 	public DeathByHealthIsZero dying;
+
+	bool winCondActive = false;
+	DeathByWinning explode;
+	WinTrigger winTrig;
+
+	void Start()
+	{
+		explode = GetComponent<DeathByWinning>();
+		winTrig = GetComponent<WinTrigger>();
+	}
 
 	public void ModifyHealth(int damage)
 	{
@@ -23,6 +32,13 @@ public class HealthSystem : MonoBehaviour {
 		{
 			//Destroy(this.gameObject);
 			StartCoroutine(dying.Death());
+		}
+
+		if((stools == mtools && sweapons == mweapons) && !winCondActive)
+		{
+			winCondActive = true;
+			explode.enabled = true;
+			winTrig.enabled = true;
 		}
 	}
 }
